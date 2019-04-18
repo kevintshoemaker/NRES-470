a85e638d515fa3a46dd7bf4b8b72f7d5c1832fb6
Shoemaker <kshoemaker@agnt.unr.edu>
2019-04-17 18:23:33 -0700

    minor

--- LECTURE15.R
+++ LECTURE15.R
@@ -19,38 +19,102 @@
 # Cormack-Jolly-Seber (CJS) model in R
 ###########
 
-library(mra)      # install the 'mra' package if you haven't already done this!
-data("dipper.data")
-head(dipper.data,10)
+library(marked)      # install the 'marked' package if you haven't already done this!
+data("dipper")
+head(dipper,10)
 
 
-data(dipper.histories)      # load data
-ct <- as.factor( paste("T",1:ncol(dipper.histories), sep=""))      # specify which years were modeled
-attr(ct,"nan")<-nrow(dipper.histories)  # assign the proper number of individuals
+##########
+# load data!
+
+data(dipper)
+
+#############
+# Process data
+
+dipper.proc=process.data(dipper,model="cjs",begin.time=1)  # Helper function- process the data for CJS model
+dipper.ddl=make.design.data(dipper.proc)    # another helper function- process data!
+
+##########
+# Fit models
+
+# fit time-varying cjs model
+
+mod.Phit.pt <- crm(dipper.proc,dipper.ddl,model.parameters=list(Phi=list(formula=~time),p=list(formula=~time)),method="Nelder-Mead",hessian = T)
+
+mod.Phit.pt   # print out model
+mod.Phit.pt$results$AIC       # extract AIC
+
+########
+# fit time-invariant cjs model
+
+mod.Phidot.pdot <- crm(dipper.proc,dipper.ddl,model.parameters = list(Phi=list(formula=~1),p=list(formula=~1)),method="Nelder-Mead",hessian = TRUE)
+
+mod.Phidot.pdot
+mod.Phidot.pdot$results$AIC
+
+########
+# fit sex-dependent cjs model
+
+mod.Phisex.psex <- crm(dipper.proc,dipper.ddl,model.parameters = list(Phi=list(formula=~sex),p=list(formula=~sex)),method="Nelder-Mead",hessian = TRUE)
+
+mod.Phisex.psex
+mod.Phisex.psex$results$AIC
+
+###########
+# compare all models with AIC
+###########
 
 ######
-# Run the model!
+# Set up models to run (must have either "Phi." or "p." in the name)
+Phi.dot <- list(formula=~1)       
+Phi.time <- list(formula=~time)
+Phi.sex <- list(formula=~sex)
+Phi.timesex <- list(formula=~sex+time)
+p.dot <- list(formula=~1)
+p.time <- list(formula=~time)
+p.sex <- list(formula=~sex)
+p.timesex <- list(formula=~sex+time)
 
-dipper.cjs <- F.cjs.estim( ~tvar(ct,drop=c(1,2)), ~tvar(ct,drop=c(1,6,7)), dipper.histories )
+cml=create.model.list(c("Phi","p"))    # create list of all models to run
 
-#####
-# Display the results
+######
+# Run all models
 
-cat("survival estimates!\n")
-dipper.cjs$s.hat[1,] 
+allmodels <- crm.wrapper(cml,data=dipper.proc, ddl=dipper.ddl,external=FALSE,accumulate=FALSE,method="Nelder-Mead",hessian=TRUE)
 
-cat("\n\n detection probability estimates!\n")
-dipper.cjs$p.hat[1,] 
+######
+# AIC model selection
+
+allmodels
+
+#######
+# get parameter estimates and confidence intervals for best model
+
+allmodels[[1]]
+
+allmodels[[11]]
+
+#######
+# make predictions and plot them. 
+
+predict(allmodels[[1]])$Phi
+
+Phi_by_year <- predict(allmodels[[11]])$Phi    # predict Phi for all years (based on the best Phi(t) model)
+
+library(Hmisc)   #load Hmisc package- has a nice error bar function
+plot(1:nrow(Phi_by_year),Phi_by_year$estimate,xlab="Year",ylab="Survival",ylim=c(0,1),main="Variability in Survival, dipper demo")
+errbar(1:nrow(Phi_by_year),Phi_by_year$estimate,Phi_by_year$ucl,Phi_by_year$lcl,add=T)
 
 
 #######
 # Compute the mean survival rate for the population
 
-mean(dipper.cjs$s.hat[1,],na.rm=T)
+mean(Phi_by_year$estimate)
 
 
 #######
 # Compute the environmental variablility in annual survival rates. 
 
-sd(dipper.cjs$s.hat[1,],na.rm=T)
+sd(Phi_by_year$estimate)
 
