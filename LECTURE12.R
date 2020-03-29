5af2cfd7cc0e24526c87b9630ac367337828ed70
Kevin <kevintshoemaker@gmail.com>
2020-03-29 16:33:55 -0700

    update

--- LECTURE12.R
+++ LECTURE12.R
@@ -19,6 +19,10 @@
 # Demonstration PVA
 ##############
 
+# STEP 1: conceptualize life history (we are modeling this population as a simple, single-stage stochastic model with density dependence)
+
+# STEP 2: parameterize the model
+
 ####
 # Basic life history parameters
 ####
@@ -31,14 +35,14 @@ K <- 175            # Carrying capacity
 # Environmental stochasticity
 ####
 
-SD_lambda <- 0.11  # standard deviation of lambda
+SD_anngrowth <- 0.11  # standard deviation of annual growth rate
 
 ####
 # Density-dependence (Ricker model)
 ####
 
 Ricker <- function(prev_abund){       # this is a function for computing next-year abundance -- includes env stochasticity
-  prev_abund * exp(log(rnorm(1,R_max,SD_lambda))*(1-(prev_abund/K)))
+  prev_abund * exp(log(rnorm(1,R_max,SD_anngrowth))*(1-(prev_abund/K)))
 }
 
 ####
@@ -48,6 +52,10 @@ Ricker <- function(prev_abund){       # this is a function for computing next-ye
 Flood_prob <- 0.05      # 5% chance of major flood
 Flood_lambda <- 0.25    # 25% of population can survive a flood 
 
+
+# STEP 3: add spatial structure (not applicable here)
+
+# STEP 4: simulate!
 
 ####
 # Basic simulation parameters
@@ -59,14 +67,14 @@ nreps <- 500      # number of replicates
 
 PVAdemo <- function(nreps,nyears,Init_N,R_max,K,Flood_prob,Flood_lambda){
   #browser()
-  PopArray2 <- array(0,dim=c((nyears+1),nreps))
+  PopArray2 <- array(0,dim=c((nyears+1),nreps))   # set up storage array
   
   ## start looping through replicates
   
   for(rep in 1:nreps){
     
     # set initial abundance
-    PopArray2[1,rep] <- Init_N     # initial abundance
+    PopArray2[1,rep] <- Init_N     # set the initial abundance
     
     ### loop through years
     for(y in 2:(nyears+1)){
@@ -87,6 +95,11 @@ PVAdemo <- function(nreps,nyears,Init_N,R_max,K,Flood_prob,Flood_lambda){
 Default <- PVAdemo(nreps,nyears,Init_N,R_max,K,Flood_prob,Flood_lambda)
 
 
+# STEP 5: results 
+
+############
+# Graphical visualization
+
 PlotCloud <- function(simdata){
   plot(c(1:101),simdata[,1],col=gray(0.7),type="l",ylim=c(0,max(simdata)),xlab="Years",ylab="Abundance")
   
@@ -96,6 +109,9 @@ PlotCloud <- function(simdata){
 }
 
 PlotCloud(Default)
+
+
+# Visualize extinction rates over time
 
 Extinction_byyear <- function(simdata){
   apply(simdata,1,function(t)  length(which(t==0)))/ncol(simdata)
@@ -107,9 +123,13 @@ abline(h=0.05,col="red",lwd=2)
 
 
 
+# visualize final abundance after 100 years relative to initial abundance
+
 hist(Default[nrow(Default),],xlab="Final abundance after 100 years",ylab="Number of replicates",main="")
 abline(v=Init_N,col="green",lwd=2)
 
+
+# plot probabilities of different severities of decline
 
 declines <- seq(0,100,by=1)
 declineprob <- numeric(length(declines))
@@ -122,6 +142,8 @@ plot(declines,declineprob,type="l",lwd=2,xlab="Decline threshold (percent)",ylab
 
 abline(v=25,col="red",lwd=2)
 
+
+# Plot extinction risk as a function of flood severity
 
 Exctinction_risk <- function(simdata){
   length(which(simdata[nrow(simdata),]==0))/ncol(simdata)
@@ -137,4 +159,5 @@ for(scenario in 1:length(flood_lambdas)){
 
 plot(flood_lambdas,all_scenarios,type="p",cex=2,xlab="flood impact (lambda in flood year)",ylab="extinction risk")
 abline(h=0.05,col="red",lwd=2)
+
 
