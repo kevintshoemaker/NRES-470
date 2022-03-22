20cf97ca4e1bd912765e797ac919018beee5b367
Kevin T Shoemaker <kevinshoemaker@unr.edu>
2022-03-21 17:37:40 -0700

    minor

--- test1.R
+++ test1.R
@@ -23,3 +23,23 @@ plot(N,b,type="l",col="green",ylim=c(0,1),lwd=2)
 lines(N,d,col="red",lwd=2)
 
 legend("topright",col=c("green","red"),lwd=2,legend=c("births","deaths"))
+
+
+popsize=500
+for(i in 1:100){
+  print(all(rbinom(popsize,1,0.75)))
+}
+
+
+red = 20
+blue = 20
+for(i in 1:100){
+  red = rpois(1,red)
+  blue = rpois(1,blue)
+  cat(paste("red is :",red,"\n"))
+  cat(paste("blue is :",blue,"\n"))
+}
+
+
+
+
